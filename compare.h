cb6ddee17dad3684b068bdc9b238f942fb6de884
M4GNV5 <jakob@m4gnus.de>
2017-06-25 00:15:10 +0200

    move the CMP_ functions to a seperate file

---
+++ compare.h
@@ -0,0 +1,20 @@
+#ifndef _CPPFUN_COMPARE_H
+#define _CPPFUN_COMPARE_H
+
+//equal
+#define CMP_E(X, Y) ISZERO(XOR(X, Y))
+//not equal
+#define CMP_NE(X, Y) B_NOT(CMP_E(X, Y))
+
+//above or equal
+#define CMP_AE(X, Y) HEAD(ADC(X, NOT(Y), 1))
+//below
+#define CMP_B(X, Y) B_NOT(CMP_AE(X, Y))
+
+//above
+#define _CMP_A(res) HEAD(res), ISNONZERO(TAIL(res))
+#define CMP_A(X, Y) _CMP_A(ADC(X, NOT(Y), 1))
+//below or equal
+#define CMP_BE(X, Y) B_NOT(CMP_A(X, Y))
+
+#endif
--- math.h
+++ math.h
@@ -3,23 +3,7 @@
 
 #include "common.h"
 #include "bitwise.h"
-
-//equal
-#define CMP_E(X, Y) ISZERO(XOR(X, Y))
-//not equal
-#define CMP_NE(X, Y) NOT(CMP_E(X, Y))
-
-//above or equal
-#define CMP_AE(X, Y) HEAD(ADC(X, NOT(Y), 1))
-//below
-#define CMP_B(X, Y) B_NOT(CMP_AE(X, Y))
-
-//above
-#define _CMP_A(res) HEAD(res), ISNONZERO(TAIL(res))
-#define CMP_A(X, Y) _CMP_A(ADC(X, NOT(Y), 1))
-//below or equal
-#define CMP_BE(X, Y) B_NOT(CMP_A(X, Y))
-
+#include "compare.h"
 
 #define ADDBIT_0_0_0 0, 0
 #define ADDBIT_0_0_1 0, 1
